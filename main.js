fafd4d473edc5af7f8e7e1e09d4b2afe26e11d57
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-04-12 18:59:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const followingElement = document.querySelector('#following');
     const linkElement = document.querySelector('#link');
 
-    fetch('https://github.com/Luizferreira01')
+    fetch('https://api.github.com/users/Luizferreira01')
     .then(function(res) {
         return res.json();
     })
