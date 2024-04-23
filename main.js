4e7b4367ece237b362a8202e771d880788d375c2
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-04-23 19:07:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const nameElement = document.querySelector('#name');
-    const usernameElement = document.querySelector('username');
+    const usernameElement = document.querySelector('#username');
     const avatarElement = document.querySelector('#avatar');
     const reposElement = document.querySelector('#repos');
     const followersElement = document.querySelector('#followers');
